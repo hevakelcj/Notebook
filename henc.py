8c4d056fc425d874c8ee6980b91d66410c8d72f9
Hevake Lee <hevakelcj@gmail.com>
2014-12-12 12:03:42 +0800

    First commit.

---
+++ henc.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+
+import os
+import base64
+
+class HEnc:
+    _openssl_template = 'openssl enc -%s -in "%s" -out "%s" -k "%s"'
+
+    # Encode or decode file
+    # \param enc_dec only can be 'e' or 'd'
+    # \param src_filename
+    # \param des_filename
+    # \param passwd
+    # \param cipher
+    @staticmethod
+    def openssl_enc_dec(enc_dec, src_filename, des_filename, passwd, cipher):
+        command = HEnc._openssl_template % (enc_dec, src_filename, des_filename, passwd)
+        if cipher != '':
+            command += ' -' + cipher
+        #print(command)
+        os.system(command)
+
+    def __init__(self):
+        self._ciphers_sequeue = 'camellia-256-ofb|seed-cfb|des-ede3-cfb8|blowfish|rc4|aes-256-xts|idea|base64'
+        self._passwd = ''
+
+    def SetPasswd(self, passwd):
+        self._passwd = passwd
+        pass
+
+    @staticmethod
+    def gen_key(passwd):
+        key = passwd 
+        while len(key) < 64:
+            key = base64.b64encode(key).strip('=')
+        return key 
+
+    def GetCipherAndKeyList(self):
+        ciphers = self._ciphers_sequeue.split('|')
+        tbl = []
+        for cipher in ciphers:
+            cipher = cipher.strip()
+            key = HEnc.gen_key(cipher + self._passwd)
+            tbl.append((cipher, key))
+        return tbl
+
+    def Encode(self, src_filename, des_filename):
+        cipher_key = self.GetCipherAndKeyList()
+        tmp_filename_src = 'temp1'
+        tmp_filename_des = 'temp2'
+        os.system('cp "%s" "%s"' % (src_filename, tmp_filename_src))
+        for (cipher, key) in cipher_key:
+            HEnc.openssl_enc_dec('e', tmp_filename_src, tmp_filename_des, key, cipher)
+            os.system('mv "%s" "%s"' % (tmp_filename_des, tmp_filename_src))
+        os.system('mv "%s" "%s"' % (tmp_filename_src, des_filename))
+
+    def Decode(self, src_filename, des_filename):
+        cipher_key = self.GetCipherAndKeyList()
+        cipher_key.reverse()
+        tmp_filename_src = 'temp1'
+        tmp_filename_des = 'temp2'
+        os.system('cp "%s" "%s"' % (src_filename, tmp_filename_src))
+        for (cipher, key) in cipher_key:
+            HEnc.openssl_enc_dec('d', tmp_filename_src, tmp_filename_des, key, cipher)
+            os.system('mv "%s" "%s"' % (tmp_filename_des, tmp_filename_src))
+        os.system('mv "%s" "%s"' % (tmp_filename_src, des_filename))
+
+#-----------------------------------------------------------------------------------------------
+def test():
+    print('===test===')
+    context = 'Hello'
+    passwd = 'abc'
+    f = open('test.txt', 'w')
+    f.write(context)
+    f.close()
+
+    henc = HEnc()
+    henc.SetPasswd(passwd)
+    henc.Encode('test.txt', 'test.enc')
+    os.unlink('test.txt')
+    henc.Decode('test.enc', 'test.txt')
+    os.unlink('test.enc')
+    f = open('test.txt', 'r')
+    context_now = f.read()
+    f.close()
+
+    if context == context_now:
+        print('Pass')
+    else:
+        print('Fail')
+    print('===done===')
+
+if __name__ == '__main__':
+    test()
---
+++ notebook.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+#coding = gbk
+
+'''
+This python script is use for encrypting our secret text file.
+It depends on openssl.
+
+File    : notebook.py 
+Author  : Chunjun Li <hevakelcj@gmail.com>
+Date    : 2014-12-01
+
+[2014-12-01 V1.0] create and first commit.
+'''
+
+import sys
+import os
+import getpass
+import henc
+
+#===============================================================================
+usage_template = '''\
+Usage: %s create|read|write|chpwd notename\
+'''
+
+ciphers = '-camellia-256-ofb'
+openssl_command = 'openssl enc %s -in "%s" -out "%s" -k "%s" -a ' + ciphers
+
+#===============================================================================
+def enc_file(txt_filename, enc_filename, passwd):
+    if not os.path.exists(txt_filename):
+        pass
+    e = henc.HEnc()
+    e.SetPasswd(passwd)
+    e.Encode(txt_filename, enc_filename)
+    del e
+
+def dec_file(enc_filename, txt_filename, passwd):
+    if not os.path.exists(enc_filename):
+        pass
+    e = henc.HEnc()
+    e.SetPasswd(passwd)
+    e.Decode(enc_filename, txt_filename)
+    del e
+
+#===============================================================================
+def get_txt_filename(notename):
+    return notename + '.txt' 
+
+def get_enc_filename(notename):
+    return notename + '.enc' 
+
+def get_passwd():
+    return getpass.getpass('Enter password: ')
+
+def get_new_passwd():
+    while True:
+        passwd = getpass.getpass('Enter new password: ')
+        if passwd == getpass.getpass('  Confirm password: '):
+            break
+        print('WARN: verify failure!')
+    return passwd
+
+def ask_yes_or_no(prompt, default):
+    if default:
+        return not raw_input(prompt + ' [yes]/no: ') == 'no'
+    else:
+        return raw_input(prompt + ' yes/[no]: ') == 'yes'
+
+#===============================================================================
+def create_notebook(notename):
+    txt_filename = get_txt_filename(notename)
+    enc_filename = get_enc_filename(notename)
+
+    if os.path.exists(enc_filename) and \
+       not ask_yes_or_no('Notebook exists! Need overwrite?', False):
+        return
+
+    passwd = get_new_passwd()
+    os.system('touch "%s"; vi "%s"' % (txt_filename, txt_filename))
+    enc_file(txt_filename, enc_filename, passwd)
+    os.unlink(txt_filename)
+
+def open_notebook(notename, is_save):
+    txt_filename = get_txt_filename(notename)
+    enc_filename = get_enc_filename(notename)
+
+    if not os.path.exists(enc_filename):
+        print('ERROR: %s doesn\'t exist!' % enc_filename)
+        return
+
+    passwd = get_passwd()
+    dec_file(enc_filename, txt_filename, passwd)
+    os.system('vi "%s"' % txt_filename)
+    if is_save:
+        if ask_yes_or_no('Is need save this modifiction?', True):
+            enc_file(txt_filename, enc_filename, passwd)
+            print('Saved.')
+        else:
+            print('Cancel.')
+    os.unlink(txt_filename)
+ 
+def read_notebook(notename):
+    open_notebook(notename, False)
+
+def write_notebook(notename):
+    open_notebook(notename, True)
+
+def change_passwd(notename):
+    txt_filename = get_txt_filename(notename)
+    enc_filename = get_enc_filename(notename)
+
+    if not os.path.exists(enc_filename):
+        print('ERROR: %s doesn\'t exist!' % enc_filename)
+        return
+
+    old_passwd = get_passwd()
+    dec_file(enc_filename, txt_filename, old_passwd)
+    os.system('vi "%s"' % txt_filename)
+    if ask_yes_or_no('Are you really want change password?', False):
+        new_passwd = get_new_passwd()
+        enc_file(txt_filename, enc_filename, new_passwd)
+        print('Done.')
+    else:
+        print('Cancel.')
+    os.unlink(txt_filename)
+
+#===============================================================================
+def notebook(operate, notename):
+    switch_operate = {'create':create_notebook,\
+                      'read'  :read_notebook,\
+                      'write' :write_notebook,\
+                      'chpwd' :change_passwd}
+    try:
+        switch_operate[operate](notename)
+    except KeyError:
+        print('ERROR: operate should be "%s".' % '|'.join(switch_operate.keys()))
+
+#===============================================================================
+def display_usage():
+    print(usage)
+
+#===============================================================================
+def test_enc_dec_file():
+    print('test_enc_dec_file')
+    txt_filename = 'test.txt'
+    enc_filename = 'test.enc'
+    context = 'Hello'
+    passwd = '1234567890'
+
+    f = open(txt_filename, 'w')
+    f.write(context)
+    f.close()
+
+    enc_file(txt_filename, enc_filename, passwd)
+    if not os.path.exists(enc_filename):
+        print('TEST ERROR: %s not exist.' % enc_filename)
+        return;
+    os.unlink(txt_filename)
+    dec_file(enc_filename, txt_filename, passwd)
+    if not os.path.exists(txt_filename):
+        print('TEST ERROR: %s not exist.' % txt_filename)
+        return;
+
+    f = open(txt_filename, 'r')
+    context_dec = f.read()
+    f.close()
+
+    if context == context_dec:
+        print('Pass')
+    else:
+        print('Fail')
+
+    os.unlink(txt_filename)
+    os.unlink(enc_filename)
+
+    print('done')
+
+def test():
+    test_enc_dec_file()
+    #TODO
+
+#===============================================================================
+if __name__ == '__main__':
+    usage = usage_template % sys.argv[0]
+    try:
+        if len(sys.argv) == 2 and sys.argv[1] == 'test':
+            test()
+        elif len(sys.argv) == 3:
+            notebook(sys.argv[1], sys.argv[2])
+        else:
+            print('ERROR: argv is not enough')
+            display_usage()
+    except KeyboardInterrupt:
+        print('\nUser interrupt.')
